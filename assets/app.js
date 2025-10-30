c76265dab52559bdc971b556a1a5afb9dc1b878d
Amin El-Rifai <amin@users.noreply.github.com>
2025-10-30 15:51:36 +0100

    Replaced inline JS, with external app.js file.

---
+++ assets/app.js
@@ -0,0 +1,7 @@
+const burger = document.querySelector('.nav-button');
+const nav = document.querySelector('.nav-container');
+
+burger.addEventListener('click', function () {
+    nav.classList.toggle('menu-open');
+    burger.classList.toggle('menu-button-active');
+});
